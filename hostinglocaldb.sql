4d0e65647823383590a031530abf15ee315759ac
ruiz-daniel <64662682+ruiz-daniel@users.noreply.github.com>
2021-02-15 17:26:16 -0500

    Update hostinglocaldb.sql
    database update

--- hostinglocaldb.sql
+++ hostinglocaldb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 08, 2021 at 11:10 PM
+-- Generation Time: Feb 15, 2021 at 11:25 PM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -60,29 +60,8 @@ CREATE TABLE `client` (
 --
 
 INSERT INTO `client` (`id`, `name`, `last_name`, `email`, `phone`, `type`) VALUES
-(87, 'dan', 'ruiz', 'ruizd1710@gmail.com', '72064388', 'Natural'),
-(88, 'dan', 'ruiz', 'ruizd1710@gmail.com', '71161351', 'Natural');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `contract`
---
-
-CREATE TABLE `contract` (
-  `id` int(7) NOT NULL,
-  `packet_id` int(8) NOT NULL,
-  `extra_disk_space` int(5) NOT NULL,
-  `extra_db_space` int(5) NOT NULL,
-  `client_id` int(7) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `contract`
---
-
-INSERT INTO `contract` (`id`, `packet_id`, `extra_disk_space`, `extra_db_space`, `client_id`) VALUES
-(61, 1, 0, 0, 88);
+(93, 'daniel', 'ruiz garcia', 'ruizd1710@gmail.com', '72064388', 'Natural'),
+(94, 'david', 'carvajal iznaga', 'ruizd1710@gmail.com', '72353090', 'Natural');
 
 -- --------------------------------------------------------
 
@@ -106,41 +85,13 @@ INSERT INTO `database_server` (`name`, `id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `hosted_site`
---
-
-CREATE TABLE `hosted_site` (
-  `id` int(11) NOT NULL,
-  `web_server` int(8) NOT NULL,
-  `php_version` varchar(5) NOT NULL,
-  `uses_nodejs` tinyint(1) NOT NULL DEFAULT 0,
-  `db_server` int(8) NOT NULL,
-  `db_password` varchar(50) NOT NULL,
-  `site_id` int(8) NOT NULL,
-  `template_id` int(8) NOT NULL,
-  `template_version` varchar(5) NOT NULL,
-  `protected_dir` text NOT NULL,
-  `index_name` varchar(12) NOT NULL,
-  `ldap_user_id` int(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `hosted_site`
---
-
-INSERT INTO `hosted_site` (`id`, `web_server`, `php_version`, `uses_nodejs`, `db_server`, `db_password`, `site_id`, `template_id`, `template_version`, `protected_dir`, `index_name`, `ldap_user_id`) VALUES
-(48, 1, '5.6', 1, 1, 'asfaf', 40, 1, '4', '', 'index.html', 42);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `ldap_user`
 --
 
 CREATE TABLE `ldap_user` (
   `id` int(8) NOT NULL,
   `username` varchar(30) NOT NULL,
-  `client_id` int(7) NOT NULL,
+  `site_id` int(8) NOT NULL,
   `password` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -148,8 +99,9 @@ CREATE TABLE `ldap_user` (
 -- Dumping data for table `ldap_user`
 --
 
-INSERT INTO `ldap_user` (`id`, `username`, `client_id`, `password`) VALUES
-(42, 'www.test.nat.cuftp', 88, 'asgagag');
+INSERT INTO `ldap_user` (`id`, `username`, `site_id`, `password`) VALUES
+(45, 'testftp', 43, 'aqwfraqfa'),
+(46, 'test2ftp', 44, 'asdfaff');
 
 -- --------------------------------------------------------
 
@@ -179,6 +131,27 @@ INSERT INTO `packet` (`id`, `name`, `disk_space`, `db_space`, `client_type`) VAL
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `quota`
+--
+
+CREATE TABLE `quota` (
+  `id` int(7) NOT NULL,
+  `packet_id` int(8) NOT NULL,
+  `extra_disk_space` int(5) NOT NULL,
+  `extra_db_space` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `quota`
+--
+
+INSERT INTO `quota` (`id`, `packet_id`, `extra_disk_space`, `extra_db_space`) VALUES
+(66, 2, 300, 200),
+(67, 1, 100, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `site`
 --
 
@@ -186,15 +159,29 @@ CREATE TABLE `site` (
   `id` int(8) NOT NULL,
   `name` varchar(30) NOT NULL,
   `alias` varchar(20) NOT NULL,
-  `client_id` int(7) NOT NULL
+  `client_id` int(7) NOT NULL,
+  `quota_id` int(8) NOT NULL,
+  `index_name` varchar(30) NOT NULL DEFAULT 'index.html',
+  `protected_dir` varchar(100) NOT NULL,
+  `web_server_id` int(8) NOT NULL,
+  `php_version` varchar(30) NOT NULL,
+  `node_js` tinyint(1) NOT NULL DEFAULT 0,
+  `db_server_id` int(8) NOT NULL,
+  `db_name` varchar(30) NOT NULL,
+  `db_user` varchar(30) NOT NULL,
+  `db_password` varchar(30) NOT NULL,
+  `template_id` int(8) NOT NULL,
+  `template_version` varchar(30) NOT NULL,
+  `hosted` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `site`
 --
 
-INSERT INTO `site` (`id`, `name`, `alias`, `client_id`) VALUES
-(40, 'test.nat.cu', 'prueba', 88);
+INSERT INTO `site` (`id`, `name`, `alias`, `client_id`, `quota_id`, `index_name`, `protected_dir`, `web_server_id`, `php_version`, `node_js`, `db_server_id`, `db_name`, `db_user`, `db_password`, `template_id`, `template_version`, `hosted`) VALUES
+(43, 'www.test.nat.cu', 'prueba', 93, 66, 'index.html', '', 1, '7.2', 0, 1, 'testdb', 'testdbo', 'asfqawq', 1, '4', 0),
+(44, 'www.test2.nat.cu', 'prueba2', 94, 67, 'index.html', '', 1, '7.2', 0, 1, 'test2db', 'test2dbo', 'asgfasfa', 2, '2', 0);
 
 -- --------------------------------------------------------
 
@@ -234,36 +221,17 @@ ALTER TABLE `client`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `contract`
---
-ALTER TABLE `contract`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `contract_ibfk_1` (`client_id`),
-  ADD KEY `packet_id` (`packet_id`);
-
---
 -- Indexes for table `database_server`
 --
 ALTER TABLE `database_server`
   ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
--- Indexes for table `hosted_site`
---
-ALTER TABLE `hosted_site`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `hosted_site_db_server` (`db_server`),
-  ADD KEY `hosted_site_web_server` (`web_server`),
-  ADD KEY `hosted_site_ldap` (`ldap_user_id`),
-  ADD KEY `hosted_site_site` (`site_id`),
-  ADD KEY `hosted_site_template` (`template_id`);
-
---
 -- Indexes for table `ldap_user`
 --
 ALTER TABLE `ldap_user`
   ADD PRIMARY KEY (`id`) USING BTREE,
-  ADD KEY `client_id` (`client_id`);
+  ADD KEY `client_id` (`site_id`);
 
 --
 -- Indexes for table `packet`
@@ -272,11 +240,22 @@ ALTER TABLE `packet`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `quota`
+--
+ALTER TABLE `quota`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `packet_id` (`packet_id`);
+
+--
 -- Indexes for table `site`
 --
 ALTER TABLE `site`
   ADD PRIMARY KEY (`id`) USING BTREE,
-  ADD KEY `site_ibfk_1` (`client_id`);
+  ADD KEY `site_ibfk_1` (`client_id`),
+  ADD KEY `site_quota_fk` (`quota_id`),
+  ADD KEY `site_database_fk` (`db_server_id`),
+  ADD KEY `site_web_server_fk` (`web_server_id`),
+  ADD KEY `site_template_fk` (`template_id`);
 
 --
 -- Indexes for table `template`
@@ -292,25 +271,13 @@ ALTER TABLE `template`
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
-
---
--- AUTO_INCREMENT for table `contract`
---
-ALTER TABLE `contract`
-  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
-
---
--- AUTO_INCREMENT for table `hosted_site`
---
-ALTER TABLE `hosted_site`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
 
 --
 -- AUTO_INCREMENT for table `ldap_user`
 --
 ALTER TABLE `ldap_user`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `packet`
@@ -319,10 +286,16 @@ ALTER TABLE `packet`
   MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT for table `quota`
+--
+ALTER TABLE `quota`
+  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+
+--
 -- AUTO_INCREMENT for table `site`
 --
 ALTER TABLE `site`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `template`
@@ -335,33 +308,26 @@ ALTER TABLE `template`
 --
 
 --
--- Constraints for table `contract`
---
-ALTER TABLE `contract`
-  ADD CONSTRAINT `contract_ibfk_2` FOREIGN KEY (`packet_id`) REFERENCES `packet` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `contract_ibfk_3` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `hosted_site`
---
-ALTER TABLE `hosted_site`
-  ADD CONSTRAINT `hosted_site_db_server` FOREIGN KEY (`db_server`) REFERENCES `database_server` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `hosted_site_ibfk_1` FOREIGN KEY (`site_id`) REFERENCES `site` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `hosted_site_ibfk_2` FOREIGN KEY (`ldap_user_id`) REFERENCES `ldap_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `hosted_site_template` FOREIGN KEY (`template_id`) REFERENCES `template` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `hosted_site_web_server` FOREIGN KEY (`web_server`) REFERENCES `available_web_server` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Constraints for table `ldap_user`
 --
 ALTER TABLE `ldap_user`
-  ADD CONSTRAINT `ldap_user_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `ldap_site_fk` FOREIGN KEY (`site_id`) REFERENCES `site` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `quota`
+--
+ALTER TABLE `quota`
+  ADD CONSTRAINT `quota_ibfk_2` FOREIGN KEY (`packet_id`) REFERENCES `packet` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `site`
 --
 ALTER TABLE `site`
-  ADD CONSTRAINT `site_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `site_client_fk` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `site_database_fk` FOREIGN KEY (`db_server_id`) REFERENCES `database_server` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `site_quota_fk` FOREIGN KEY (`quota_id`) REFERENCES `quota` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `site_template_fk` FOREIGN KEY (`template_id`) REFERENCES `template` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `site_web_server_fk` FOREIGN KEY (`web_server_id`) REFERENCES `available_web_server` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
